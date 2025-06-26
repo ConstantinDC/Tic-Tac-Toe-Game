721f48d3fcf16d3252d4c58a3f96fb145773102e
Constantin <stilus.office@gmail.com>
2025-06-26 07:13:48 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,69 @@
+const board = document.getElementById('board');
+const cells = document.getElementsByClassName('cell');
+const players = ['X', 'O'];
+let currentPlayer = players[0];
+const endMessage = document.createElement('h2');
+endMessage.textContent = `X turn`;
+board.after(endMessage);
+endMessage.style.textAlign='center';
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
+for (let i = 0; i < cells.length; ++i) {
+  cells[i].addEventListener('click', () => {
+    if (cells[i].textContent !== '') {
+      return; 
+    }
+    cells[i].textContent = currentPlayer; 
+    if (checkWin(currentPlayer)) {
+      endMessage.textContent = `Game over! ${currentPlayer} wins!`;
+      return; 
+    }
+    if (checkTie()) {
+      endMessage.textContent = `Game is tied!`;
+      return; 
+    }
+    currentPlayer = (currentPlayer === players[0]) ? players[1] : players[0];
+    if (currentPlayer == players[0]) {
+      endMessage.textContent = `X turn`;
+    } else {
+      endMessage.textContent = `O turn`; 
+    }
+  });
+}
+
+function checkWin(currentPlayer) {
+  for (let i = 0; i < winningCombinations.length; ++i) {
+    const [a, b, c] = winningCombinations[i];
+    if (cells[a].textContent === currentPlayer && cells[b].textContent === currentPlayer && cells[c].textContent === currentPlayer) {
+      return true; 
+    } 
+  }
+  return false;
+}
+
+function checkTie() {
+  for (let i = 0; i < cells.length; ++i) {
+    if (cells[i].textContent === '') {
+      return false;
+    }
+  }
+  return true; 
+}
+
+function restartButton() {
+  for (let i = 0; i < cells.length; ++i) {
+    cells[i].textContent = ''; 
+  }
+  endMessage.textContent = `X turn!`;
+  currentPlayer = players[0];
+}
+
